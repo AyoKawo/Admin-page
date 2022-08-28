521099f4976ad823d30f8b108018c00554712228
AyoKawo <109248710+AyoKawo@users.noreply.github.com>
2022-08-28 21:54:31 +0100

    Add files via upload

---
+++ order.js
@@ -0,0 +1,43 @@
+const Orders = [
+  {
+    productName: "Mt Kawo",
+    productNumber: "56789",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+
+  {
+    productName: "Sl Aremu",
+    productNumber: "98765",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+
+  {
+    productName: "Mv Gusau",
+    productNumber: "34526",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+
+  {
+    productName: "Mt Ayo",
+    productNumber: "23421",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+
+  {
+    productName: "Sl Bolu",
+    productNumber: "32456",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+
+  {
+    productName: "St Yungest",
+    productNumber: "23526",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+];
